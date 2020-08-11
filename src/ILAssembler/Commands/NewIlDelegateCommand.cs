fa9247d94c8319384cbad49b904a75427664e8b5
Patrick Meinecke <SeeminglyScience@users.noreply.github.com>
2020-08-10 21:41:38 -0400

    Fix signature bugs (#3)
    * Fix ctors and fields from generic types
    
    * Fix method resolution for generic methods with params
    
    * Fix exception when auto delegate is Action

--- src/ILAssembler/Commands/NewIlDelegateCommand.cs
+++ src/ILAssembler/Commands/NewIlDelegateCommand.cs
@@ -6,8 +6,9 @@ using System.Reflection.Emit;
 
 namespace ILAssembler.Commands
 {
-    [Cmdlet(VerbsCommon.New, "IlDelegate", DefaultParameterSetName = ByAstSignatureSet), Alias("il")]
-    public class NewIlDelegateCommand : PSCmdlet
+    [Cmdlet(VerbsCommon.New, "IlDelegate", DefaultParameterSetName = ByAstSignatureSet)]
+    [Alias("il")]
+    public sealed class NewIlDelegateCommand : PSCmdlet
     {
         private const string ByAstSignatureSet = "ByAstSignature";
 
@@ -28,64 +29,64 @@ namespace ILAssembler.Commands
         protected override void BeginProcessing()
         {
             DynamicMethod method;
-            if (DelegateType is not null)
-            {
-                if (!DelegateType.IsSubclassOf(typeof(Delegate)))
-                {
-                    ThrowTerminatingError(
-                        new ErrorRecord(
-                            new PSArgumentException(
-                                "The specified delegate type does not inherit from \"System.Delegate\".",
-                                nameof(DelegateType)),
-                            "InvalidDelegateType",
-                            ErrorCategory.InvalidArgument,
-                            DelegateType));
-                    return;
-                }
-
-                var invokeMethod = DelegateType.GetMethod(nameof(Action.Invoke));
-                if (invokeMethod is null)
-                {
-                    ThrowTerminatingError(
-                        new ErrorRecord(
-                            new PSArgumentException(
-                                "The specified delegate type does not have an usable \"Invoke\" method.",
-                                nameof(DelegateType)),
-                            "InvalidDelegateType",
-                            ErrorCategory.InvalidArgument,
-                            DelegateType));
-                    return;
-                }
-
-                method = new DynamicMethod(
-                    invokeMethod.Name,
-                    invokeMethod.ReturnType,
-                    invokeMethod.GetParameters().Select(p => p.ParameterType).ToArray(),
-                    restrictedSkipVisibility: true);
-            }
-            else
-            {
-                var signatureParser = new MethodSignatureParser(
-                    rejectCtor: true,
-                    requireResolvableDeclaringType: false);
-
-                Signature!.Ast.Visit(signatureParser);
-                var signature = (MethodIdentifier)signatureParser.GetMemberIdentifier(Signature!.Ast.Extent);
-
-                DelegateType = DelegateTypeFactory.GetDelegateType(
-                    signature,
-                    shouldCreateDelegate: false,
-                    Signature.Ast.Extent);
-
-                method = new DynamicMethod(
-                    signature.Name,
-                    signature.ReturnType.GetModifiedType(),
-                    signature.Parameters.ToModifiedTypeArray(),
-                    restrictedSkipVisibility: true);
-            }
-
             try
             {
+                if (DelegateType is not null)
+                {
+                    if (!DelegateType.IsSubclassOf(typeof(Delegate)))
+                    {
+                        ThrowTerminatingError(
+                            new ErrorRecord(
+                                new PSArgumentException(
+                                    "The specified delegate type does not inherit from \"System.Delegate\".",
+                                    nameof(DelegateType)),
+                                "InvalidDelegateType",
+                                ErrorCategory.InvalidArgument,
+                                DelegateType));
+                        return;
+                    }
+
+                    var invokeMethod = DelegateType.GetMethod(nameof(Action.Invoke));
+                    if (invokeMethod is null)
+                    {
+                        ThrowTerminatingError(
+                            new ErrorRecord(
+                                new PSArgumentException(
+                                    "The specified delegate type does not have an usable \"Invoke\" method.",
+                                    nameof(DelegateType)),
+                                "InvalidDelegateType",
+                                ErrorCategory.InvalidArgument,
+                                DelegateType));
+                        return;
+                    }
+
+                    method = new DynamicMethod(
+                        invokeMethod.Name,
+                        invokeMethod.ReturnType,
+                        invokeMethod.GetParameters().Select(p => p.ParameterType).ToArray(),
+                        restrictedSkipVisibility: true);
+                }
+                else
+                {
+                    var signatureParser = new MethodSignatureParser(
+                        rejectCtor: true,
+                        requireResolvableDeclaringType: false);
+
+                    Signature!.Ast.Visit(signatureParser);
+                    var signature = (MethodIdentifier)signatureParser.GetMemberIdentifier(Signature!.Ast.Extent);
+
+                    DelegateType = DelegateTypeFactory.GetDelegateType(
+                        signature,
+                        shouldCreateDelegate: false,
+                        Signature.Ast.Extent);
+
+                    method = new DynamicMethod(
+                        signature.Name,
+                        signature.ReturnType.GetModifiedType(),
+                        signature.Parameters.ToModifiedTypeArray(),
+                        restrictedSkipVisibility: true);
+                }
+
                 CilAssembler.CompileTo(
                     (ScriptBlockAst)Body!.Ast,
                     method);
@@ -98,6 +99,7 @@ namespace ILAssembler.Commands
                         "ILParseException",
                         ErrorCategory.InvalidArgument,
                         null));
+                return;
             }
 
             WriteObject(method.CreateDelegate(DelegateType));
