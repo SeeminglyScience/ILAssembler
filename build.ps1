d2f2e6d2a3a06bdaab4495f9e22962a655e8a2c7
Patrick Meinecke <seeminglyscience@gmail.com>
2020-10-04 08:48:30 -0400

    try to fix ubuntu

--- build.ps1
+++ build.ps1
@@ -36,6 +36,42 @@ end {
         $null = Import-Module @importModuleSplat
     }
 
+    # Seems like PSDepend is failing to install from the gallery in the github
+    # actions ubuntu image. As a band-aid I'm just gonna repeat the logic here
+    # until I figure out a proper fix.
+    if ($IsLinux) {
+        $reqs = Import-PowerShellDataFile $PSScriptRoot/requirements.psd1
+        foreach ($req in $reqs.GetEnumerator()) {
+            if ($req.Key -match 'Dotnet') {
+                continue
+            }
+
+            $importModuleSplat = @{
+                MinimumVersion = $req.Value
+                Force = $true
+                ErrorAction = 'Ignore'
+                PassThru = $true
+                Name = $req.Key
+            }
+
+            if (-not (Import-Module @importModuleSplat)) {
+                $installModuleSplat = @{
+                    MinimumVersion = $req.Value
+                    Scope = 'CurrentUser'
+                    AllowClobber = $true
+                    AllowPrerelease = $true
+                    SkipPublisherCheck = $true
+                    Force = $true
+                    Name = $req.Key
+                }
+
+                Install-Module @installModuleSplat -ErrorAction Stop
+                $importModuleSplat['ErrorAction'] = 'Stop'
+                $null = Import-Module @importModuleSplat
+            }
+        }
+    }
+
     Invoke-PSDepend -Path $PSScriptRoot/requirements.psd1 -Import -Install -Force:$Force -ErrorAction Stop
 
     if ($Publish) {
