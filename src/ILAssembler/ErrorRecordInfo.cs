67c652376b0b857e0ee00f0d2003e5d99dbc2832
Patrick Meinecke <seeminglyscience@gmail.com>
2020-10-04 15:55:16 -0400

    don't use records

--- src/ILAssembler/ErrorRecordInfo.cs
+++ src/ILAssembler/ErrorRecordInfo.cs
@@ -2,5 +2,19 @@ using System.Management.Automation;
 
 namespace ILAssembler
 {
-    internal record ErrorRecordInfo(string Id, ErrorCategory Category, object? TargetObject);
+    internal sealed class ErrorRecordInfo
+    {
+        public ErrorRecordInfo(string id, ErrorCategory category, object? targetObject)
+        {
+            Id = id;
+            Category = category;
+            TargetObject = targetObject;
+        }
+
+        public string Id { get; }
+
+        public ErrorCategory Category { get; }
+
+        public object? TargetObject { get; }
+    }
 }
